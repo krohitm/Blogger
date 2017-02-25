27aa0127cba06cdc5f0ac5051117d163d16c39cc
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-02-25 03:59:04 -0500

    updated image size

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
-	has_attached_file :image
+	has_many :attachments
+	has_attached_file :image, styles: { medium: "300*300>", thumb: "100*100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 	def tag_list
 		self.tags.collect do |tag|
